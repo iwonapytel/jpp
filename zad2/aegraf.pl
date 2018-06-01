613333d29782dd6da2e0cd3e0c5939dc8a54c015
Iwona Pytel <ip360730@students.mimuw.edu.pl>
2018-06-01 22:22:48 +0200

    upgrade code

--- zad2/aegraf.pl
+++ zad2/aegraf.pl
@@ -1,56 +1,54 @@
 :- use_module(library(lists)).
 
-znajdzWierzcholek([[V | L] | _], V, [V | L]) :- !.
+znajdzWierzcholek([Vertex | _], V, Vertex) :- Vertex = [V | _], !.
 znajdzWierzcholek([_ | T], V, Vertex) :- znajdzWierzcholek(T, V, Vertex).
 
-wyborWierzcholka([X, T], [X, T]).
-wyborWierzcholka([X, e | Y1], [X, e, Y2])  :- member(Y2, Y1).
-wyborWierzcholka([X, a | Y1], [X, a | Y2]) :- permutation(Y2, Y1), !.
+wyborWierzcholka([V, Type], [V, Type]).
+wyborWierzcholka([V, e | N1], [V, e, N2])  :- member(N2, N1).
+wyborWierzcholka([V, a | N1], [V, a | N2]) :- permutation(N2, N1), !.
 
 % jestWyborem(+AEgraf, -Graf)
 jestWyborem(_, []).
-jestWyborem([X | R1], [Y | R2]) :- wyborWierzcholka(X, Y), !, jestWyborem(R1, R2).
-jestWyborem(AEgraf, [[V | Tail] | R]) :-
-	znajdzWierzcholek(AEgraf, V, Vertex),
-	wyborWierzcholka(Vertex, [V | Tail]),
-	jestWyborem(AEgraf, R).
 
-% ?- jestWyborem([[v1, a, v2, v3], [v2, a, v1, v4]], X).
-% ?- jestWyborem([[v2, a, v1, v3], [v1, a, v2, v3]], [[v1, a, v2, v3], [v2, a, v1, v3]]).
+jestWyborem([V1 | T1], [V2 | T2]) :-
+	wyborWierzcholka(V1, V2), !,
+	jestWyborem(T1, T2).
+
+jestWyborem(AEgraf, [[V | N] | T]) :-
+	znajdzWierzcholek(AEgraf, V, Vertex),
+	wyborWierzcholka(Vertex, [V | N]),
+	jestWyborem(AEgraf, T).
+
 
 % jestDFS(+Graf, -Lista)
-
-
 jestDFS([], []).
-jestDFS([[V | X] | Y], Lista) :- doDFS([[V | X] | Y], znajdzSasiadowAE, [V], [], Lista).
+
+jestDFS(Graf, Lista) :- Graf = [[V | _] | _], doDFS(Graf, znajdzSasiadowAE, [V], [], Lista).
 
 % DFS(Graf, Stos, Odwiedzone, Lista)
-
 doDFS(_, _, [], L, L).
+
 doDFS(Graf, ZnajdzSasiadow, [S | Stos], Odwiedzone, Lista) :-
 	\+ member(S, Odwiedzone), !,
-	append(Odwiedzone, [S], NOdwiedzone),
+	append(Odwiedzone, [S], _Odwiedzone),
 	call(ZnajdzSasiadow, Graf, S, Sasiedzi),
-	append(Sasiedzi, Stos, NStos),
-        doDFS(Graf, ZnajdzSasiadow, NStos, NOdwiedzone, Lista).
+	append(Sasiedzi, Stos, _Stos),
+        doDFS(Graf, ZnajdzSasiadow, _Stos, _Odwiedzone, Lista).
+
 doDFS(Graf, ZnajdzSasiadow, [S | Stos], Odwiedzone, Lista) :-
 	member(S, Odwiedzone),
 	doDFS(Graf, ZnajdzSasiadow, Stos, Odwiedzone, Lista).
-	
 
-%?- doDFS([[v0, a, v1, v3], [v1, e, v3], [v2, a], [v3, a]], [v0], [], X).
-
-znajdzSasiadowAE([[V, _ | L1] | _], V, L1) :- !.
-znajdzSasiadowAE([_ | L2], V, Sasiedzi) :- znajdzSasiadowAE(L2, V, Sasiedzi).
-
-% todo zwroc dowolny, a nie tylko pierwszy 
-znajdzSasiadowWybor([[V, a| L1] | _], V, L1).
-znajdzSasiadowWybor([[V, e] | _], V, []).
-znajdzSasiadowWybor([[V, e, X | _]| _], V, [X]).
-znajdzSasiadowWybor([_ | R], V, Sasiedzi) :- znajdzSasiadowWybor(R, V, Sasiedzi).
+% znajdzSasiadow(+Graf, +V, -Sasiedzi)
+znajdzSasiadowAE(Graf, V, Sasiedzi) :- znajdzWierzcholek(Graf, V, [_, _ | Sasiedzi]).
+ 
+znajdzSasiadowWybor(Graf, V, Sasiedzi) :- znajdzWierzcholek(Graf, V, [_, a | Sasiedzi]).
+znajdzSasiadowWybor(Graf, V, []) :- znajdzWierzcholek(Graf, V, [_, e]).
+znajdzSasiadowWybor(Graf, V, [N]) :-
+	znajdzWierzcholek(Graf, V, [_, e | Sasiedzi]),
+	member(N, Sasiedzi).
 
 % JestADFS(+AEgraf, -Lista)
-
 jestADFS([], []).
 jestADFS(AEgraf, Lista) :-
 	jestWyborem(AEgraf, Wybor),
@@ -58,7 +56,6 @@ jestADFS(AEgraf, Lista) :-
 
 % JestADFS1(+AEgraf, -Lista)
 jestADFS1([], []).
-jestADFS1([[V | X] | Y], Lista) :-
-	doDFS([[V | X] | Y], znajdzSasiadowWybor, [V], [], Lista). 	
-	
-% jestADFS1([[v1, a, v2], [v2, e, v3, v4, v5], [v3, e], [v4, e], [v5, e]], X).
+jestADFS1(AEgraf, Lista) :-
+	AEgraf = [[V | _] | _],
+	doDFS(AEgraf, znajdzSasiadowWybor, [V], [], Lista).
